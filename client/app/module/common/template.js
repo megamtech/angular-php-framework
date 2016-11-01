31d5b762be32a017b81fe03bf379645179261d85
BalajiV <balajiv@megamtech.com>
2016-11-01 15:15:14 +0530

    Bug fixes on the framework migration Task #160 - Update to the latest code

--- client/app/module/common/template.js
+++ client/app/module/common/template.js
@@ -2,11 +2,11 @@
 
     'use strict';
     angular
-            .module('1grandtrunk')
+            .module('megamapp')
             .controller('TemplateController', TemplateController);
-    function TemplateController(UIService, UserService, $state, $rootScope, APIService, $window, $cookies) {
+    function TemplateController(UIService, UserService, $state, $rootScope) {
         var vm = this;
-        vm.appname = ' - 1GrandTrunk';
+        vm.appname = ' - MegamFrameWork';
         $rootScope.helpstartstep = 0;
         vm.isUserAuthenticated = function () {
             if (UserService.isUserAuthenticated()) {
@@ -14,8 +14,6 @@
             } else {
                 return false;
             }
-
-
         };
         vm.getUserDetails = function () {
             return UserService.getUserDetails();
@@ -27,18 +25,6 @@
             if (angular.isDefined($state.$current.data))
                 if (angular.isDefined($state.$current.data.title))
                     return $state.$current.data.title + vm.appname;
-        };
-        vm.updateHelp = function (show_guide) {
-            $cookies.put('show_guide', show_guide);
-            if (show_guide == true) {
-                $window.location.reload();
-            }
-            APIService.post('userguide', '', {show_guide: show_guide}).success(function (data) {
-
-            });
-        };
-        vm.showHelpGuide = function () {
-            return $cookies.get('show_guide');
         };
     }
 })();
