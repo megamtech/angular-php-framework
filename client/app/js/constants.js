98bfc832b3688749da9f9c7f4d83a6c7fad83cc3
BalajiV <balajiv@megamtech.com>
2016-11-01 15:57:20 +0530

    Bug fixes on the framework migration Task #160 - Update to the latest code

--- client/app/js/constants.js
+++ client/app/js/constants.js
@@ -1,18 +1,18 @@
 /**
-url pattern : [[module_name]]:{
-	[[action_type]]:[[API Signature of API / URL]]
-}
-*/
+ url pattern : [[module_name]]:{
+ [[action_type]]:[[API Signature of API / URL]]
+ }
+ */
 var url = 'http://127.0.0.1/api/server/';
 (function () {
     'use strict';
     angular
             .module('megamapp').constant('API_URLs', {
-        
+        'register': url + 'register',
+        'login': url + 'login',
         "module_name": {
             "add": url + 'master/add',
         }
-
     }).constant('DataFormat', {
         'date': {
             'medium': 'd MMM yyyy hh:mm a',
